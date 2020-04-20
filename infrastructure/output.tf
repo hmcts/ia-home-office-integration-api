7d2217688c7b44752548ce8d2c05dbd640105f89
Ravi Aghera <Ravi.Aghera@HMCTS.net:wq>
2020-04-20 17:34:48 +0100

    Helm: Adding infra config

---
+++ infrastructure/output.tf
@@ -0,0 +1,15 @@
+output "microserviceName" {
+  value = var.component
+}
+
+output "resourceGroup" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "vaultName" {
+  value = local.key_vault_name
+}
+
+output "vaultUri" {
+  value = data.azurerm_key_vault.ia_key_vault.vault_uri
+}
