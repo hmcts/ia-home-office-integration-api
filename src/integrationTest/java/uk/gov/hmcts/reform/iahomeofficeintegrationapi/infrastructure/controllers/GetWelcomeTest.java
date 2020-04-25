e6a52d586531f0a89e6ae636f0e74ebe6ac13d0e
Ravi Aghera <Ravi.Aghera@HMCTS.net:wq>
2020-04-25 16:18:58 +0100

    S2S: fixing integration test

--- src/integrationTest/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/controllers/GetWelcomeTest.java
@@ -1,21 +1,38 @@
 package uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure.controllers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.Application;
 
-@WebMvcTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = MOCK)
+@ActiveProfiles("integration")
 public class GetWelcomeTest {
 
-    @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setUp() {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
 
     @DisplayName("Should welcome upon root request with 200 response code")
     @Test
