3e0d201a3893e4f8408851e3258da788791ac269
Ravi Aghera <Ravi.Aghera@HMCTS.net:wq>
2020-05-01 19:57:25 +0100

    Adding Dispatcher and Handler framework to App

--- src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/controllers/PreSubmitCallbackControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/controllers/PreSubmitCallbackControllerTest.java
@@ -1,19 +1,66 @@
 package uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure.controllers;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.CaseDetails;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure.PreSubmitCallbackDispatcher;
 
+@ExtendWith(MockitoExtension.class)
 public class PreSubmitCallbackControllerTest {
 
-    private static final String hoUAN = "1111-2222-3333-4444";
-    private final PreSubmitCallbackController controller = new PreSubmitCallbackController();
+    @Mock private PreSubmitCallbackDispatcher<AsylumCase> callbackDispatcher;
+    @Mock private PreSubmitCallbackResponse<AsylumCase> callbackResponse;
+    @Mock private Callback<AsylumCase> callback;
+    @Mock private CaseDetails<AsylumCase> caseDetails;
+
+    private PreSubmitCallbackController preSubmitCallbackController;
+
+    @BeforeEach
+    public void setUp() {
+        preSubmitCallbackController =
+            new PreSubmitCallbackController(
+                callbackDispatcher
+            );
+    }
 
     @Test
-    public void should_call_with_homeoffice_reference_return_value() {
-        String response = controller.ccdAboutToSubmit(hoUAN).toString();
+    public void should_dispatch_about_to_submit_callback_then_return_response() {
 
-        assertThat(response.contains(hoUAN));
+        when(callback.getCaseDetails()).thenReturn(caseDetails);
 
+        doReturn(callbackResponse)
+            .when(callbackDispatcher)
+            .handle(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
+
+        ResponseEntity<PreSubmitCallbackResponse<AsylumCase>> actualResponse =
+            preSubmitCallbackController.ccdAboutToSubmit(callback);
+
+
+        verify(callbackDispatcher, times(1)).handle(
+            PreSubmitCallbackStage.ABOUT_TO_SUBMIT,
+            callback
+        );
+    }
+
+    @Test
+    public void should_not_allow_null_constructor_arguments() {
+
+        assertThatThrownBy(() -> new PreSubmitCallbackController(null))
+            .hasMessage("callbackDispatcher must not be null")
+            .isExactlyInstanceOf(NullPointerException.class);
     }
 }
