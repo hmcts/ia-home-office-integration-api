cab1552ba8b00b91b9ec9f83394a54101a041f61
divya.arun <divya.arun@enginegroup.com>
2020-07-16 11:43:17 +0100

    RIA-3376:Add test for error scenario

--- src/contractTest/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/HomeOfficeConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/HomeOfficeConsumerTest.java
@@ -124,6 +124,42 @@ public class HomeOfficeConsumerTest {
     }
 
     @Pact(provider = "homeoffice_api", consumer = "hmcts")
+    public RequestResponsePact executeSearchByParametersAndGet400(PactDslWithProvider builder) {
+
+        Map<String, String> headers = Maps.newHashMap();
+        headers.put(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        //DO NOT CHANGE THE INDENTATION OF THE BODY
+        PactDslJsonBody searchErrorResponseBody = new PactDslJsonBody();
+        searchErrorResponseBody
+            .object("messageHeader")
+            .stringType("correlationId", "ABC2344BCED2234EA")
+            .stringType("eventDateTime", "2017-07-21T17:32:28Z")
+            .object("consumer")
+            .stringMatcher("code", "HMCTS", "HMCTS")
+            .stringType("description", "HM Courts and Tribunal Service")
+            .closeObject()
+            .closeObject()
+            .object("errorDetail")
+            .stringType("errorCode", "1100")
+            .stringType("messageText",
+                "Invalid reference format. Format should be either nnnn-nnnn-nnnn-nnnn or 0(0) followed by digits (total length 9 or 10)")
+            .booleanType("success", false)
+            .closeObject()
+            .closeObject();
+
+        return builder
+            .given("Home Office sends error code after receiving HMCTS search request")
+            .uponReceiving("Provider receives a POST /applicationStatus/getBySearchParameters request from an IA API")
+            .path(HOMEOFFICE_API_SEARCH_URL)
+            .headers(headers)
+            .method(HttpMethod.POST.toString())
+            .willRespondWith()
+            .status(HttpStatus.BAD_REQUEST.value())
+            .body(searchErrorResponseBody)
+            .toPact();
+    }
+
+    @Pact(provider = "homeoffice_api", consumer = "hmcts")
     public RequestResponsePact executeSetInstructAndGet200(PactDslWithProvider builder) {
 
         Map<String, String> headers = Maps.newHashMap();
@@ -150,6 +186,42 @@ public class HomeOfficeConsumerTest {
             .willRespondWith()
             .status(HttpStatus.OK.value())
             .body(instructResponseBody)
+            .toPact();
+    }
+
+    @Pact(provider = "homeoffice_api", consumer = "hmcts")
+    public RequestResponsePact executeSetInstructAndGet400(PactDslWithProvider builder) {
+
+        Map<String, String> headers = Maps.newHashMap();
+        headers.put(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        //DO NOT CHANGE THE INDENTATION OF THE BODY
+        PactDslJsonBody errorInstructResponseBody = new PactDslJsonBody();
+        errorInstructResponseBody
+            .object("messageHeader")
+            .stringType("correlationId", "ABC2344BCED2234EA")
+            .stringType("eventDateTime", "2017-07-21T17:32:28Z")
+            .object("consumer")
+            .stringMatcher("code", "HMCTS", "HMCTS")
+            .stringType("description", "HM Courts and Tribunal Service")
+            .closeObject()
+            .closeObject()
+            .object("errorDetail")
+            .stringType("errorCode", "1100")
+            .stringType("messageText",
+                "Invalid reference format. Format should be either nnnn-nnnn-nnnn-nnnn or 0(0) followed by digits (total length 9 or 10)")
+            .booleanType("success", false)
+            .closeObject()
+            .closeObject();
+
+        return builder
+            .given("Home Office sends error code after receiving HMCTS notification")
+            .uponReceiving("Provider receives a POST /applicationInstruct/setInstruct request from an IA API")
+            .path(HOMEOFFICE_API_INSTRUCT_URL)
+            .headers(headers)
+            .method(HttpMethod.POST.toString())
+            .willRespondWith()
+            .status(HttpStatus.BAD_REQUEST.value())
+            .body(errorInstructResponseBody)
             .toPact();
     }
 
@@ -236,6 +308,43 @@ public class HomeOfficeConsumerTest {
     }
 
     @Test
+    @PactTestFor(pactMethod = "executeSearchByParametersAndGet400")
+    public void should_get_error_detail_for_searching_invalid_ho_reference(MockServer mockServer) throws JSONException {
+
+        String actualResponseBody =
+            SerenityRest
+                .given()
+                .relaxedHTTPSValidation()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post(mockServer.getUrl() + HOMEOFFICE_API_SEARCH_URL)
+                .then()
+                .statusCode(400)
+                .and()
+                .extract()
+                .body()
+                .asString();
+
+        JSONObject response = new JSONObject(actualResponseBody);
+
+        assertThat(actualResponseBody).isNotNull();
+        assertThat(response).hasNoNullFieldsOrProperties();
+        assertThat(response.getJSONObject("messageHeader")).isNotNull();
+        assertThat(response.getJSONObject("messageHeader").getString("correlationId")).isNotBlank();
+        assertThat(response.getJSONObject("messageHeader").getString("eventDateTime")).isNotBlank();
+        assertThat(response.getJSONObject("messageHeader").getJSONObject("consumer")).isNotNull();
+        assertThat(response.getJSONObject("messageHeader").getJSONObject("consumer").getString("code"))
+            .matches("HMCTS");
+        assertThat(response.getJSONObject("messageHeader").getJSONObject("consumer").getString("description"))
+            .isNotBlank();
+        assertThat(response.getJSONObject("errorDetail")).isNotNull();
+        assertThat(response.getJSONObject("errorDetail").getString("errorCode")).isNotBlank();
+        assertThat(response.getJSONObject("errorDetail").getString("messageText")).isNotBlank();
+        assertThat(response.getJSONObject("errorDetail").getBoolean("success")).isFalse();
+
+    }
+
+    @Test
     @PactTestFor(pactMethod = "executeSetInstructAndGet200")
     public void should_get_success_for_sending_notification(MockServer mockServer) throws JSONException {
 
@@ -265,6 +374,43 @@ public class HomeOfficeConsumerTest {
             .matches("HMCTS");
         assertThat(response.getJSONObject("messageHeader").getJSONObject("consumer").getString("description"))
             .isNotBlank();
+
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "executeSetInstructAndGet400")
+    public void should_get_error_detail_for_sending_invalid_notification(MockServer mockServer) throws JSONException {
+
+        String actualResponseBody =
+            SerenityRest
+                .given()
+                .relaxedHTTPSValidation()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post(mockServer.getUrl() + HOMEOFFICE_API_INSTRUCT_URL)
+                .then()
+                .statusCode(400)
+                .and()
+                .extract()
+                .body()
+                .asString();
+
+        JSONObject response = new JSONObject(actualResponseBody);
+
+        assertThat(actualResponseBody).isNotNull();
+        assertThat(response).hasNoNullFieldsOrProperties();
+        assertThat(response.getJSONObject("messageHeader")).isNotNull();
+        assertThat(response.getJSONObject("messageHeader").getString("correlationId")).isNotBlank();
+        assertThat(response.getJSONObject("messageHeader").getString("eventDateTime")).isNotBlank();
+        assertThat(response.getJSONObject("messageHeader").getJSONObject("consumer")).isNotNull();
+        assertThat(response.getJSONObject("messageHeader").getJSONObject("consumer").getString("code"))
+            .matches("HMCTS");
+        assertThat(response.getJSONObject("messageHeader").getJSONObject("consumer").getString("description"))
+            .isNotBlank();
+        assertThat(response.getJSONObject("errorDetail")).isNotNull();
+        assertThat(response.getJSONObject("errorDetail").getString("errorCode")).isNotBlank();
+        assertThat(response.getJSONObject("errorDetail").getString("messageText")).isNotBlank();
+        assertThat(response.getJSONObject("errorDetail").getBoolean("success")).isFalse();
 
     }
 }
