d50672bb35b027292dda878a13920e5fc4fd2a85
divisathyan <divya.arun@enginegroup.com>
2020-10-15 11:29:11 +0100

    RIA-3545: Handle different error scenario (#130)
    * RIA-3545: Handle different error scenario paths
    
    * RIA-3545:Add functional test for generic error

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/client/HomeOfficeResponseException.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/client/HomeOfficeResponseException.java
@@ -2,8 +2,19 @@ package uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure.client;
 
 public class HomeOfficeResponseException extends RuntimeException {
 
-    public HomeOfficeResponseException(String message) {
+    private final String errorCode;
+
+    public HomeOfficeResponseException(final String errorCode, String message) {
         super(message);
+        this.errorCode = errorCode;
     }
 
+    public HomeOfficeResponseException(String message) {
+        super(message);
+        errorCode = null;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
 }
