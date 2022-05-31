581d4dd21f1e296610cfe93ad41146d847a10a7e
indu2019 <indu.chaturvedi@hmcts.net>
2022-05-31 11:22:38 +0100

    RIA-5679 committing few changes so that I dont lose them

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/controllers/PreSubmitCallbackController.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/controllers/PreSubmitCallbackController.java
@@ -3,8 +3,6 @@ package uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure.controller
 import static java.util.Objects.requireNonNull;
 import static org.springframework.http.ResponseEntity.ok;
 
-import javax.validation.constraints.NotNull;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -12,6 +10,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -51,35 +50,35 @@ public class PreSubmitCallbackController {
             summary = "Handles 'AboutToStartEvent' callbacks from CCD",
             security =
                     {
-                            @SecurityRequirement(name = "Authorization"),
-                            @SecurityRequirement(name = "ServiceAuthorization")
+                        @SecurityRequirement(name = "Authorization"),
+                        @SecurityRequirement(name = "ServiceAuthorization")
                     },
             responses =
                     {
-                            @ApiResponse(
-                                    responseCode = "200",
-                                    description = "Transformed Asylum case data, with any identified error or warning messages",
-                                    content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
+                        @ApiResponse(
+                            responseCode = "200",
+                            description = "Transformed Asylum case data, with any identified error or warning messages",
+                            content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
 
-                            @ApiResponse(
-                                    responseCode = "400",
-                                    description = "Bad Request",
-                                    content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
+                        @ApiResponse(
+                             responseCode = "400",
+                             description = "Bad Request",
+                             content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
 
-                            @ApiResponse(
-                                    responseCode = "403",
-                                    description = "Forbidden",
-                                    content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
+                        @ApiResponse(
+                              responseCode = "403",
+                              description = "Forbidden",
+                              content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
 
-                            @ApiResponse(
-                                    responseCode = "415",
-                                    description = "Unsupported Media Type",
-                                    content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
+                        @ApiResponse(
+                              responseCode = "415",
+                              description = "Unsupported Media Type",
+                              content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
 
-                            @ApiResponse(
-                                    responseCode = "500",
-                                    description = "Internal Server Error",
-                                    content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class)))
+                        @ApiResponse(
+                              responseCode = "500",
+                              description = "Internal Server Error",
+                              content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class)))
 
                     }
     )
@@ -98,43 +97,43 @@ public class PreSubmitCallbackController {
                     + "warning messages",
             security =
                     {
-                            @SecurityRequirement(name = "Authorization"),
-                            @SecurityRequirement(name = "ServiceAuthorization")
+                        @SecurityRequirement(name = "Authorization"),
+                        @SecurityRequirement(name = "ServiceAuthorization")
                     },
             responses =
                     {
-                            @ApiResponse(
-                                    responseCode = "200",
-                                    description = "Optional confirmation text for CCD UI",
-                                    content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
+                        @ApiResponse(
+                              responseCode = "200",
+                              description = "Optional confirmation text for CCD UI",
+                              content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
 
-                            @ApiResponse(
-                                    responseCode = "400",
-                                    description = "Bad Request",
-                                    content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
+                        @ApiResponse(
+                              responseCode = "400",
+                              description = "Bad Request",
+                              content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
 
-                            @ApiResponse(
-                                    responseCode = "403",
-                                    description = "Forbidden",
-                                    content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
+                        @ApiResponse(
+                               responseCode = "403",
+                               description = "Forbidden",
+                               content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
 
-                            @ApiResponse(
-                                    responseCode = "415",
-                                    description = "Unsupported Media Type",
-                                    content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
+                        @ApiResponse(
+                               responseCode = "415",
+                               description = "Unsupported Media Type",
+                               content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class))),
 
 
-                            @ApiResponse(
-                                    responseCode = "500",
-                                    description = "Internal Server Error",
-                                    content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class)))
+                        @ApiResponse(
+                                responseCode = "500",
+                                description = "Internal Server Error",
+                                content = @Content(schema = @Schema(implementation = PreSubmitCallbackResponse.class)))
 
                     }
     )
 
     @PostMapping(path = "/ccdAboutToSubmit")
     public ResponseEntity<PreSubmitCallbackResponse<AsylumCase>> ccdAboutToSubmit(
-            @Parameter(name = "Asylum case data", required = true) @NotNull @RequestBody Callback<AsylumCase> callback) {
+           @Parameter(name = "Asylum case data", required = true) @NotNull @RequestBody Callback<AsylumCase> callback) {
         System.out.println("Home office PreSubmitCallbackResponse/ccdAboutToSubmit");
         return performStageRequest(PreSubmitCallbackStage.ABOUT_TO_SUBMIT, callback);
 
