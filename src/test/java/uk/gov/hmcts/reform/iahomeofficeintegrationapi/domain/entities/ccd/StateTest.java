bc69f03ac82a9c0ad3f3132a748fe4b65e40a70f
Jamie <jamiemurphy@version1.com>
2023-02-13 15:32:12 +0000

    Fix state test

--- src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/StateTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/StateTest.java
@@ -30,6 +30,6 @@ public class StateTest {
 
     @Test
     public void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(17, State.values().length);
+        assertEquals(18, State.values().length);
     }
 }
