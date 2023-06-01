969e450f471dd17b50a5f7a0eb2b0cafb38e2345
Filippo Possenti <filippo.possenti@version1.com>
2023-06-01 16:15:58 +0100

    RIA-7251 Temporary change to inspect environment

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/ConfigValidatorAppListener.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/ConfigValidatorAppListener.java
@@ -1,13 +1,20 @@
 package uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,6 +22,9 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 public class ConfigValidatorAppListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    Environment env;
 
     @Value("${auth.homeoffice.client.baseUrl}")
     private String homeOfficeBaseUrl;
@@ -30,9 +40,9 @@ public class ConfigValidatorAppListener implements ApplicationListener<ContextRe
     }
 
     void breakOnMissingHomeOfficeSecrets() {
+        log.info("Home office base URL: {}", homeOfficeBaseUrl);
+        printEnvironment();
         if (StringUtils.isBlank(clientId)) {
-            log.info("Home office base URL: {}", homeOfficeBaseUrl);
-            printEnvironment();
             throw new IllegalArgumentException("auth.homeoffice.client.id is null or empty."
                 + " This is not allowed and it will break production. This is a secret value stored in a vault"
                 + " (unless running locally). Check application.yaml for further information.");
@@ -54,5 +64,16 @@ public class ConfigValidatorAppListener implements ApplicationListener<ContextRe
         for (Map.Entry<String, String> entry : environment.entrySet()) {
             log.info("    {} = {}", entry.getKey(), entry.getValue());
         }
+
+        log.info("Spring environment properties: ");
+        for (PropertySource<?> source : ((AbstractEnvironment) env).getPropertySources()) {
+            if (source instanceof MapPropertySource) {
+                Map<String, Object> props = ((MapPropertySource) source).getSource();
+                for (Map.Entry<String, Object> entry : props.entrySet()) {
+                    log.info("    {} = {}", entry.getKey(), entry.getValue());
+                }
+            }
+        }
+
     }
 }
