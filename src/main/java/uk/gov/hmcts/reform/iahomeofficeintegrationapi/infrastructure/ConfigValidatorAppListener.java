b81d05e84ab296f02b5f0443023070f4704ddfff
Filippo Possenti <filippo.possenti@version1.com>
2023-06-01 09:05:25 +0100

    RIA-7251 Detection of local environment

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/ConfigValidatorAppListener.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/ConfigValidatorAppListener.java
@@ -1,13 +1,14 @@
 package uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure;
 
-import java.util.Map;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,6 +16,9 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 public class ConfigValidatorAppListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    Environment env;
 
     @Value("${auth.homeoffice.client.baseUrl}")
     private String homeOfficeBaseUrl;
@@ -30,29 +34,22 @@ public class ConfigValidatorAppListener implements ApplicationListener<ContextRe
     }
 
     void breakOnMissingHomeOfficeSecrets() {
+        if (homeOfficeBaseUrl.contains("localhost") || homeOfficeBaseUrl.contains("127.0.0.1")) {
+            log.warn("Detected local deployment. Secrets will not be checked.");
+            return;
+        }
+        log.info("Home office base URL: {}", homeOfficeBaseUrl);
         if (StringUtils.isBlank(clientId)) {
-            log.info("Home office base URL: {}", homeOfficeBaseUrl);
-            printEnvironment();
             throw new IllegalArgumentException("auth.homeoffice.client.id is null or empty."
                 + " This is not allowed and it will break production. This is a secret value stored in a vault"
                 + " (unless running locally). Check application.yaml for further information.");
         }
         if (StringUtils.isBlank(clientSecret)) {
             log.info("Home office base URL: {}", homeOfficeBaseUrl);
-            printEnvironment();
             throw new IllegalArgumentException("auth.homeoffice.client.secret is null or empty."
                 + " This is not allowed and it will break production. This is a secret value stored in a vault"
                 + " (unless running locally). Check application.yaml for further information.");
 
-        }
-    }
-
-    private void printEnvironment() {
-        log.info("Environment variables: ");
-        Map<String, String> environment = System.getenv();
-
-        for (Map.Entry<String, String> entry : environment.entrySet()) {
-            log.info("    {} = {}", entry.getKey(), entry.getValue());
         }
     }
 }
