86b46a78c88a449c5dbe1772cb359ecb4b3d3517
nilay <nilay.patel@hmcts.net>
2023-10-03 13:02:32 +0100

    DIAC-52 fixes

--- src/contractTest/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/HomeOfficeConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/HomeOfficeConsumerTest.java
@@ -3,12 +3,12 @@ package uk.gov.hmcts.reform.iahomeofficeintegrationapi;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import au.com.dius.pact.consumer.MockServer;
-import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.core.model.annotations.Pact;;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
-import au.com.dius.pact.model.RequestResponsePact;
+import au.com.dius.pact.core.model.RequestResponsePact;
 import com.google.common.collect.Maps;
 import groovy.util.logging.Slf4j;
 import io.restassured.RestAssured;
