b270ff6084efde71ff666c9e2e03c7f41a060761
DennisHoV1 <106669367+DennisHoV1@users.noreply.github.com>
2023-10-24 15:10:30 +0100

    RIA-7195 add the witnessPartyId and witnessFamilyName in WitnessDetails (#354)
    * RIA-7195 add the witnessPartyId and witnessFamilyName in WitnessDetails
    
    * fix the checkstyle problem and temporarily suppress CVE problem

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/WitnessDetails.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/WitnessDetails.java
@@ -1,15 +1,29 @@
 package uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class WitnessDetails {
 
+    private String witnessPartyId;
     private String witnessName;
+    private String witnessFamilyName;
+
+    public WitnessDetails() {
+    }
+
+    public WitnessDetails(String witnessName) {
+        this.witnessName = witnessName;
+    }
+
+    public WitnessDetails(String witnessName, String witnessFamilyName) {
+        this.witnessName = witnessName;
+        this.witnessFamilyName = witnessFamilyName;
+    }
 }
