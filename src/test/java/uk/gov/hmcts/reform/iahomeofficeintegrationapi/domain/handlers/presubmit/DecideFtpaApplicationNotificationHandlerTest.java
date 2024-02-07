dcd040daa50a667279be2b110caed24d6acf2401
JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>
2024-02-07 12:21:55 +0000

    RIA-8101 DLRM judge rework - jenkins fix

--- src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/handlers/presubmit/DecideFtpaApplicationNotificationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/handlers/presubmit/DecideFtpaApplicationNotificationHandlerTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
