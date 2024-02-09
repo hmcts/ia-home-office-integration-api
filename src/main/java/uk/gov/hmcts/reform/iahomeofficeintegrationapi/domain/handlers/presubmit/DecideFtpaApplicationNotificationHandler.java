a1a628c325c69b780f2b40a3ec1d03bb9d357c82
jamieclarkeHMCTS <128475668+jamieclarkeHMCTS@users.noreply.github.com>
2024-02-09 09:30:27 +0000

    RIA-8101 judge rework ho-integration (#380)
    * RIA-8101 DLRM judge rework
    
    * RIA-8101 DLRM judge rework - jenkins fix
    
    * RIA-8101 DLRM judge rework - jenkins fix
    
    * RIA-8101 DLRM judge rework - jenkins fix
    
    * RIA-8101 DLRM judge rework - addressing comments
    
    * RIA-8101 DLRM judge rework - renaming functional tests
    
    ---------
    
    Co-authored-by: JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/handlers/presubmit/DecideFtpaApplicationNotificationHandler.java
@@ -0,0 +1,60 @@
+package uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.handlers.presubmit;
+
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.Event.DECIDE_FTPA_APPLICATION;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.handlers.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.service.FtpaDecidedNotificationsHelper;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.service.HomeOfficeInstructService;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.service.NotificationsHelper;
+
+
+@Slf4j
+@Component
+public class DecideFtpaApplicationNotificationHandler implements PreSubmitCallbackHandler<AsylumCase> {
+
+    private HomeOfficeInstructService homeOfficeInstructService;
+    private NotificationsHelper notificationsHelper;
+    private FtpaDecidedNotificationsHelper ftpaDecidedNotificationsHelper;
+
+    public DecideFtpaApplicationNotificationHandler(
+        HomeOfficeInstructService homeOfficeInstructService,
+        NotificationsHelper notificationsHelper,
+        FtpaDecidedNotificationsHelper ftpaDecidedNotificationsHelper
+    ) {
+        this.homeOfficeInstructService = homeOfficeInstructService;
+        this.notificationsHelper = notificationsHelper;
+        this.ftpaDecidedNotificationsHelper = ftpaDecidedNotificationsHelper;
+    }
+
+    @Override
+    public boolean canHandle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
+        requireNonNull(callbackStage, "callbackStage must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+               && (callback.getEvent() == DECIDE_FTPA_APPLICATION);
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<AsylumCase> handle(
+        PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
+
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+
+        ftpaDecidedNotificationsHelper.handleFtpaDecidedNotification(
+            asylumCase, notificationsHelper, homeOfficeInstructService, callback.getEvent(), "RJ_");
+
+        return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+}
