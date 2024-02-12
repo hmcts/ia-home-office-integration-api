0c03c55a521cd94a81badddf9aacf9474d20dd69
Miguel Martin <miguel.martin1@hmcts.net>
2024-02-12 08:56:15 +0100

    RIA-8104 fix linting

--- src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/DecideFtpaApplicationOutcomeTypeTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/DecideFtpaApplicationOutcomeTypeTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
 
 public class DecideFtpaApplicationOutcomeTypeTest {
 
