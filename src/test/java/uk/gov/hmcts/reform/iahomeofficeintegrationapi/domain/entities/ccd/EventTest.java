c757d123b15e25a68537ad964e792e8d4c537ce5
jamieclarkeHMCTS <128475668+jamieclarkeHMCTS@users.noreply.github.com>
2024-05-10 09:37:19 +0100

    RIA-TASK - Merge master changes to DLRM - ia-home-office-integration-api (#375)
    * base branch for dlrm
    
    * RIA-8101 DLRM judge refactor (#376)
    
    * RIA-8101 DLRM judge rework
    
    * RIA-8101 DLRM judge rework - renaming classes
    
    ---------
    
    Co-authored-by: JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>
    
    * Revert "RIA-8101 DLRM judge refactor" (#378)
    
    * RIA-8101 judge rework ho-integration (#380)
    
    * RIA-8101 DLRM judge rework
    
    * RIA-8101 DLRM judge rework - jenkins fix
    
    * RIA-8101 DLRM judge rework - jenkins fix
    
    * RIA-8101 DLRM judge rework - jenkins fix
    
    * RIA-8101 DLRM judge rework - addressing comments
    
    * RIA-8101 DLRM judge rework - renaming functional tests
    
    ---------
    
    Co-authored-by: JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>
    
    * RIA-8104 Fix sonnar issue (#382)
    
    * Empty-Commit
    
    * RIA-8104 Fix build
    
    * RIA-8104 fix linting
    
    * update suppressions.xml (#385)
    
    * update suppressions.xml (#387)
    
    * RIA-8758 DLRM rebase from master for CcdScenarioRunnerTest (#388)
    
    * RIA-8888 updated suppressions.xml (#392)
    
    * updated suppressions.xml
    
    * updated suppressions.xml
    
    * RIA-8860 Merge with master (#398)
    
    * RIA-8934 Fix configuration files before merging
    
    * RIA-8934 Fix configuration files before merging
    
    ---------
    
    Co-authored-by: JamieClarkeV1 <120718578+JamieClarkeV1@users.noreply.github.com>
    Co-authored-by: Miguel Martin <miguel.martin1@hmcts.net>
    Co-authored-by: ThomasKKC <99262639+ThomasKKC@users.noreply.github.com>
    Co-authored-by: Ryan-Nesbitt <95298094+Ryan-Nesbitt@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/EventTest.java
@@ -27,11 +27,12 @@ class EventTest {
         assertEquals("sendDirection", Event.SEND_DIRECTION.toString());
         assertEquals("requestResponseAmend", Event.REQUEST_RESPONSE_AMEND.toString());
         assertEquals("changeDirectionDueDate", Event.CHANGE_DIRECTION_DUE_DATE.toString());
+        assertEquals("decideFtpaApplication", Event.DECIDE_FTPA_APPLICATION.toString());
         assertEquals("unknown", Event.UNKNOWN.toString());
     }
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(20, Event.values().length);
+        assertEquals(21, Event.values().length);
     }
 }
