5023ce1fada911bdda19a48640829a52ebef9d67
ghs_N1J5EZLwp4ZElcnb3JrPX4g7Ulbeni3lPP8N <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2024-10-08 14:52:23 +0000

    Updating Terraform Formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,16 +4,16 @@ provider "azurerm" {
 
 locals {
 
-  preview_vault_name           = "${var.raw_product}-aat"
-  non_preview_vault_name       = "${var.raw_product}-${var.env}"
-  key_vault_name               = var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name
+  preview_vault_name     = "${var.raw_product}-aat"
+  non_preview_vault_name = "${var.raw_product}-${var.env}"
+  key_vault_name         = var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name
 
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
   location = var.location
-  tags     = merge(var.common_tags, tomap({"lastUpdated" = timestamp()}))
+  tags     = merge(var.common_tags, tomap({ "lastUpdated" = timestamp() }))
 }
 
 data "azurerm_key_vault" "ia_key_vault" {
