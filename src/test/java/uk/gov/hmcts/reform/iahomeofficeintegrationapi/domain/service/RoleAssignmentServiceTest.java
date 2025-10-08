78831939f24bdd4ab71043f621cad0759cbadb40
nilay <nilay.patel@solirius.com>
2025-10-08 15:48:31 +0100

    checkstyle

--- src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/service/RoleAssignmentServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/service/RoleAssignmentServiceTest.java
@@ -12,7 +12,6 @@ import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
