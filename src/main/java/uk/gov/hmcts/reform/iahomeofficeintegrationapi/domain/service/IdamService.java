e3f7cea98b8e21aee10b28a1ed7660dc9add45b5
Ed Graham <ed.graham@hmcts.net>
2025-10-10 13:51:09 +0100

    WIP - will this pass the build?

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/service/IdamService.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/service/IdamService.java
@@ -2,11 +2,14 @@ package uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure.client.IdamApi;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure.client.model.idam.Token;
 import uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure.client.model.idam.UserInfo;
 
 @Slf4j
@@ -38,5 +41,20 @@ public class IdamService {
         userInfo.setRoles(roles);
         return userInfo;
     }
-}
 
+    // I don't know what I am doing here
+    @Cacheable(value = "systemTokenCache")
+    public Token getServiceUserToken() {
+        Map<String, String> idamAuthDetails = new ConcurrentHashMap<>();
+
+        idamAuthDetails.put("grant_type", "password");
+        /*idamAuthDetails.put("redirect_uri", idamRedirectUrl);
+        idamAuthDetails.put("client_id", idamClientId);
+        idamAuthDetails.put("client_secret", idamClientSecret);
+        idamAuthDetails.put("username", systemUserName);
+        idamAuthDetails.put("password", systemUserPass);
+        idamAuthDetails.put("scope", systemUserScope);*/
+
+        return idamApi.token(idamAuthDetails);
+    }
+}
