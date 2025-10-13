d05f1142f9762e663e7be3c91b4d7eb7b9fbad61
Nilay <nilay.patel@solirius.com>
2025-10-13 12:54:02 +0100

    DIAC-1626 improving am get user roles call (#425)
    * improving am get user roles call
    
    * sonar fix
    
    * fix attempt 13
    
    fix attempt 12
    
    fix attempt 11
    
    fix attempt 10
    
    fix attempt 9
    
    fix attempt 8
    
    fix attempt 7
    
    error improvement
    
    fix attempt 2
    
    fix attempt
    
    Revert "AM fix"
    
    This reverts commit d1f1a324374eadbff43c52f4f803d1d5a55e0e3e.
    
    AM fix
    
    Revert "fixes"
    
    This reverts commit 8b223904f678932fbb736f42dbcf155a88187353.
    
    Revert "fixes"
    
    This reverts commit 59db6724d6f8c5b17b044cc0365689e47b2429b1.
    
    adding functional logging
    
    fixes
    
    test fix
    
    * fix attempt 14
    
    * final fix
    
    * checkstyle
    
    * test fix
    
    * Trigger Build

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/client/roleassignment/RoleAssignmentApi.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/client/roleassignment/RoleAssignmentApi.java
@@ -1,32 +1,27 @@
 package uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure.client.roleassignment;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import java.util.Map;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.roleassignment.RoleAssignmentResource;
-
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure.config.DisableHystrixFeignConfiguration;
 
 @FeignClient(
     name = "role-assignment-service-api",
-    url = "${role-assignment-service.url}"
+    url = "${role-assignment-service.url}",
+    configuration = DisableHystrixFeignConfiguration.class
 )
 public interface RoleAssignmentApi {
-    String ACTOR_ID = "actorId";
     String SERVICE_AUTHORIZATION = "ServiceAuthorization";
 
-    @GetMapping(
-        value = "/am/role-assignments/actors/{actorId}",
-        consumes = APPLICATION_JSON_VALUE,
-        headers = CONTENT_TYPE + "=" + APPLICATION_JSON_VALUE
-    )
-    RoleAssignmentResource getRoleAssignments(
-        @RequestHeader(AUTHORIZATION) String authorization,
-        @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
-        @PathVariable(ACTOR_ID) String actorId);
-
+    @PostMapping(value = "/am/role-assignments/query", consumes = "application/json")
+    RoleAssignmentResource queryRoleAssignments(
+        @RequestHeader(AUTHORIZATION) String userToken,
+        @RequestHeader(SERVICE_AUTHORIZATION) String s2sToken,
+        @RequestBody Map<String, Object> body
+    );
 }
