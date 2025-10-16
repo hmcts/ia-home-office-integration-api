04d1b7fd119eb4cec850578c54638284ad12d559
david-gimelle <david.gimelle@gmail.com>
2025-10-16 15:05:26 +0100

    update StatutoryTimeFrame24WeeksFieldValue

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/StatutoryTimeFrame24WeeksFieldValue.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/StatutoryTimeFrame24WeeksFieldValue.java
@@ -1,17 +1,45 @@
 package uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd;
 
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.field.YesOrNo;
+
+import static java.util.Objects.requireNonNull;
+
+@EqualsAndHashCode
+@ToString
 public class StatutoryTimeFrame24WeeksFieldValue {
-    private String davidToFixThisForMeMerciBeaucoup;
 
-    public StatutoryTimeFrame24WeeksFieldValue(String willNotReallyBeAString) {
-        this.davidToFixThisForMeMerciBeaucoup = willNotReallyBeAString;
+    private YesOrNo stf24wStatus;
+    private String stf24wStatusReason;
+    private String user;
+    private String dateAdded;
+
+    public StatutoryTimeFrame24WeeksFieldValue(
+        YesOrNo stf24wStatus,
+        String stf24wStatusReason,
+        String user,
+        String dateAdded
+    ) {
+        this.stf24wStatus = requireNonNull(stf24wStatus);
+        this.stf24wStatusReason = requireNonNull(stf24wStatusReason);
+        this.user = requireNonNull(user);
+        this.dateAdded = requireNonNull(dateAdded);
     }
 
-    private StatutoryTimeFrame24WeeksFieldValue() {
-
+    public YesOrNo getStf24wStatus() {
+        return requireNonNull(stf24wStatus);
     }
 
-    public String getDavidToFixThisForMeMerciBeaucoup() {
-        return davidToFixThisForMeMerciBeaucoup;
+    public String getStf24wStatusReason() {
+        return requireNonNull(stf24wStatusReason);
     }
+
+    public String getUser() {
+        return requireNonNull(user);
+    }
+
+    public String getDateAdded() {
+        return requireNonNull(dateAdded);
+     }
 }
--- src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/StatutoryTimeFrame24WeeksFieldValueTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/StatutoryTimeFrame24WeeksFieldValueTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.field.YesOrNo;
 
 @SuppressWarnings("unchecked")
 class StatutoryTimeFrame24WeeksFieldValueTest {
@@ -12,7 +13,16 @@ class StatutoryTimeFrame24WeeksFieldValueTest {
     @Test
     void has_correct_values() {
 
-        statutoryTimeFrame24WeeksFieldValue = new StatutoryTimeFrame24WeeksFieldValue("YYZ");
-        assertEquals("YYZ", statutoryTimeFrame24WeeksFieldValue.getDavidToFixThisForMeMerciBeaucoup());
+        statutoryTimeFrame24WeeksFieldValue = new StatutoryTimeFrame24WeeksFieldValue(
+            YesOrNo.YES,
+            "Test reason",
+            "Test user",
+            "2024-01-01"
+        );
+        
+        assertEquals(YesOrNo.YES, statutoryTimeFrame24WeeksFieldValue.getStf24wStatus());
+        assertEquals("Test reason", statutoryTimeFrame24WeeksFieldValue.getStf24wStatusReason());
+        assertEquals("Test user", statutoryTimeFrame24WeeksFieldValue.getUser());
+        assertEquals("2024-01-01", statutoryTimeFrame24WeeksFieldValue.getDateAdded());
     }
 }
