95352378af47203099265f3f42633b25ab63215d
david-gimelle <david.gimelle@gmail.com>
2025-10-30 13:28:24 +0000

    add more tests

--- src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/service/CcdDataServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/service/CcdDataServiceTest.java
@@ -12,11 +12,20 @@ import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.Statut
 import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.field.IdValue;
 import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.field.YesOrNo;
 import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.service.IdamService;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure.client.model.idam.UserInfo;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CcdDataServiceTest {
@@ -64,5 +73,53 @@ public class CcdDataServiceTest {
         assertThat(value.getReason()).isEqualTo("Home Office statutory timeframe update");
         assertThat(value.getUser()).isEqualTo("Home Office Integration API");
         assertThat(value.getDateTimeAdded()).isEqualTo("2023-12-25T00:00:00Z");
+    }
+
+    @Test
+    void setHomeOfficeStatutoryTimeframeStatus_shouldUpdateCaseSuccessfully() {
+        // Given
+        String caseId = "1234567890";
+        String userToken = "user-token";
+        String s2sToken = "s2s-token";
+        String userId = "user-id";
+        String eventToken = "event-token";
+        
+        HomeOfficeStatutoryTimeframeDto dto = HomeOfficeStatutoryTimeframeDto.builder()
+            .ccdCaseNumber(caseId)
+            .hoStatutoryTimeframeStatus(true)
+            .timeStamp(LocalDate.of(2023, 12, 25))
+            .build();
+        
+        StartEventResponse startEventResponse = StartEventResponse.builder()
+            .token(eventToken)
+            .build();
+        
+        CaseDetails expectedCaseDetails = CaseDetails.builder()
+            .id(Long.parseLong(caseId))
+            .callbackResponseStatus("SUCCESS")
+            .build();
+        
+        UserInfo userInfo = new UserInfo("email@test.com", userId, null, "Test User", "Test", "User");
+
+        // When
+        when(systemTokenGenerator.getServiceUserToken()).thenReturn(userToken);
+        when(serviceAuthorization.generate()).thenReturn(s2sToken);
+        when(systemTokenGenerator.getUserInfo("Bearer " + userToken)).thenReturn(userInfo);
+        when(coreCaseDataApi.startEvent(anyString(), anyString(), eq(caseId), eq(userId)))
+            .thenReturn(startEventResponse);
+        when(coreCaseDataApi.submitEventForCaseWorker(anyString(), anyString(), anyString(), 
+            anyString(), anyString(), eq(caseId), anyBoolean(), any()))
+            .thenReturn(expectedCaseDetails);
+
+        CaseDetails result = ccdDataService.setHomeOfficeStatutoryTimeframeStatus(dto);
+
+        // Then
+        assertThat(result).isEqualTo(expectedCaseDetails);
+        verify(systemTokenGenerator).getServiceUserToken();
+        verify(serviceAuthorization).generate();
+        verify(systemTokenGenerator).getUserInfo("Bearer " + userToken);
+        verify(coreCaseDataApi).startEvent("Bearer " + userToken, s2sToken, caseId, userId);
+        verify(coreCaseDataApi).submitEventForCaseWorker(eq("Bearer " + userToken), eq(s2sToken), 
+            eq(userId), eq("IA"), eq("Asylum"), eq(caseId), eq(true), any());
     }
 }
