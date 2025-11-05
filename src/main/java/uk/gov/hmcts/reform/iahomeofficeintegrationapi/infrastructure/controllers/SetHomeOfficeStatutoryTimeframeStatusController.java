d6c514267f47ae25f6107bc9446be7388bc61702
david-gimelle <david.gimelle@gmail.com>
2025-11-05 11:41:28 +0000

    remove auth

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/controllers/SetHomeOfficeStatutoryTimeframeStatusController.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/controllers/SetHomeOfficeStatutoryTimeframeStatusController.java
@@ -11,7 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.SubmitEventDetails;
 import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.HomeOfficeStatutoryTimeframeDto;
