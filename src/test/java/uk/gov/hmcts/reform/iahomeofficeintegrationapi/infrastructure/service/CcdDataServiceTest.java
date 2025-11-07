bea6b8db10c760a6d4fed55e12c5a601eaee098a
david-gimelle <david.gimelle@gmail.com>
2025-11-07 07:07:05 +0000

    restore 24 weeks event

--- src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/service/CcdDataServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/service/CcdDataServiceTest.java
@@ -108,7 +108,7 @@ class CcdDataServiceTest {
             "IA",
             "Asylum",
             "12345",
-            "addCaseNote"  // Changed from "setHomeOfficeStatutoryTimeframeStatus"
+            "setHomeOfficeStatutoryTimeframeStatus" 
         );
         verify(ccdDataApi).submitEvent(
             eq("Bearer " + userToken),
