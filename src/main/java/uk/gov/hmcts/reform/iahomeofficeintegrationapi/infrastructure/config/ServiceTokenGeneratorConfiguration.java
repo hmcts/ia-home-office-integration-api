c9f1e018567e092ea065c766e0a899245ea31248
david-gimelle <david.gimelle@gmail.com>
2026-01-27 10:37:21 +0000

    log s2s url

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/config/ServiceTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/config/ServiceTokenGeneratorConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.iahomeofficeintegrationapi.infrastructure.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +11,18 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 @Configuration
 @Lazy
+@Slf4j
 public class ServiceTokenGeneratorConfiguration {
 
     @Bean
     public AuthTokenGenerator authTokenGenerator(
         @Value("${idam.s2s-auth.totp_secret}") String secret,
         @Value("${idam.s2s-auth.microservice}") String microService,
+        @Value("${idam.s2s-auth.url}") String s2sUrl,
         ServiceAuthorisationApi serviceAuthorisationApi
     ) {
+        log.info("S2S auth URL: {}", s2sUrl); // DO NOT MERGE THIS
+        log.info("S2S microservice: {}", microService);
         return AuthTokenGeneratorFactory.createDefaultGenerator(
             secret,
             microService,
