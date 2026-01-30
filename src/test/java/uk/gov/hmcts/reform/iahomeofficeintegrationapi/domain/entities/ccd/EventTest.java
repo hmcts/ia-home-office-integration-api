fe5e88aa3efb41981629e17c1d0e905961fdaa23
Ed Graham <ed.graham@hmcts.net>
2026-01-30 18:29:56 +0000

    Fixed test

--- src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/EventTest.java
+++ src/test/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/entities/ccd/EventTest.java
@@ -34,6 +34,6 @@ class EventTest {
 
     @Test
     void if_this_test_fails_it_is_because_it_needs_updating_with_your_changes() {
-        assertEquals(23, Event.values().length);
+        assertEquals(24, Event.values().length);
     }
 }
