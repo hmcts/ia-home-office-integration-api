870eb34d5f891425beff87eb4117fab92cd103de
Ed Graham <34963216+ed-graham@users.noreply.github.com>
2026-02-24 17:10:34 +0000

    DIAC-2134 Home Office case API expansion (#451)
    * trigger build
    
    * Refactored to allow different exception handlers for each client (necessary for the 24-week code to come)

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/client/HomeOfficeSearchApi.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/client/HomeOfficeSearchApi.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.HomeOfficeSearch;
 
 
-@FeignClient(name = "home-office-case-search-api", url = "${home-office.api.url}")
+@FeignClient(name = "home-office-case-search-api", url = "${home-office.api.url}", configuration = FeignErrorDecoderConfig.class)
 public interface HomeOfficeSearchApi {
 
     @PostMapping("/ichallenge/applicationStatus/getBySearchParameters")
