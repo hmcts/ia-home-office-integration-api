5877bb9d52fc68b244cc8fb890d4e9199d341fb8
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2026-03-27 10:41:56 +0000

    fix merge conflicts

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/client/model/idam/UserInfo.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/client/model/idam/UserInfo.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,8 +20,6 @@ import lombok.Setter;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class UserInfo implements Serializable {
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class UserInfo {
 
     @Serial
     private static final long serialVersionUID = 1L;
