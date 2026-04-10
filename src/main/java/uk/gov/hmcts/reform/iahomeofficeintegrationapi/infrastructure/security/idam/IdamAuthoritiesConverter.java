5577bb635aee2ea5a9640386a98df0b75146a5ee
Sahil Goonraz <sahil.goonraz@solirius.com>
2026-04-10 11:10:40 +0100

    DIAC-447 - Remove unused import

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/security/idam/IdamAuthoritiesConverter.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/security/idam/IdamAuthoritiesConverter.java
@@ -8,7 +8,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.lang.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
