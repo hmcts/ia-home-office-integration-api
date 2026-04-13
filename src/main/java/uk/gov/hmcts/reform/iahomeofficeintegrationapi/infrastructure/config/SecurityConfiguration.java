7f354f51d7ed96985a5216efa90a87911387dd79
nilay <nilay.patel@solirius.com>
2026-04-13 14:11:03 +0100

    springboot 3.0 -> 3.1

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/config/SecurityConfiguration.java
@@ -103,9 +103,8 @@ public class SecurityConfiguration {
             .authorizeHttpRequests(requests -> requests
                 .anyRequest().authenticated())
             .oauth2ResourceServer(server -> server
-                .jwt()
-                .jwtAuthenticationConverter(jwtAuthenticationConverter)
-                .and())
+                .jwt(jwt -> jwt
+                    .jwtAuthenticationConverter(jwtAuthenticationConverter)))
             .oauth2Client(withDefaults());
         return http.build();
     }
