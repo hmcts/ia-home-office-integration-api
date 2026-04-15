397a888bf31442436a7fb186274df496305ef1fa
nilay <nilay.patel@solirius.com>
2026-04-15 10:10:24 +0100

    Replacing caffeine with redis

--- src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/config/CacheConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/infrastructure/config/CacheConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,11 +36,6 @@ public class CacheConfiguration {
     private String redisEncryptionKey;
 
     @Bean
-    public CacheManagerCustomizer<CaffeineCacheManager> cacheManagerCustomizer() {
-        return cacheManager -> cacheManager.setAllowNullValues(false);
-    }
-
-    @Bean
     @Primary
     public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
         try {
@@ -50,43 +44,50 @@ public class CacheConfiguration {
 
             // Idam user info config
             AesEncryptingRedisSerializer<UserInfo> userInfoSerializer =
-                    new AesEncryptingRedisSerializer<>(
-                            new Jackson2JsonRedisSerializer<>(UserInfo.class),
-                            redisEncryptionKey
-                    );
+                new AesEncryptingRedisSerializer<>(
+                    new Jackson2JsonRedisSerializer<>(UserInfo.class),
+                    redisEncryptionKey
+                );
 
             RedisCacheConfiguration userInfoCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
-                    .entryTtl(Duration.ofSeconds(3300))
-                    .disableCachingNullValues()
-                    .serializeKeysWith(
-                            RedisSerializationContext.SerializationPair
-                                    .fromSerializer(new StringRedisSerializer()))
-                    .serializeValuesWith(
-                            RedisSerializationContext.SerializationPair
-                                    .fromSerializer(userInfoSerializer));
+                .entryTtl(Duration.ofSeconds(3300))
+                .disableCachingNullValues()
+                .serializeKeysWith(
+                    RedisSerializationContext.SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(
+                    RedisSerializationContext.SerializationPair
+                        .fromSerializer(userInfoSerializer));
 
             // system user token config
             AesEncryptingRedisSerializer<String> tokenSerializer =
-                    new AesEncryptingRedisSerializer<>(
-                            new Jackson2JsonRedisSerializer<>(String.class),
-                            redisEncryptionKey
-                    );
+                new AesEncryptingRedisSerializer<>(
+                    new Jackson2JsonRedisSerializer<>(String.class),
+                    redisEncryptionKey
+                );
 
             RedisCacheConfiguration tokenCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
-                    .entryTtl(Duration.ofSeconds(3300))  // 55mins (token might expire before cache)
-                    .disableCachingNullValues()
-                    .serializeKeysWith(
-                            RedisSerializationContext.SerializationPair
-                                    .fromSerializer(new StringRedisSerializer()))
-                    .serializeValuesWith(
-                            RedisSerializationContext.SerializationPair
-                                    .fromSerializer(tokenSerializer));
+                .entryTtl(Duration.ofSeconds(3300))  // 55mins (token might expire before cache)
+                .disableCachingNullValues()
+                .serializeKeysWith(
+                    RedisSerializationContext.SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(
+                    RedisSerializationContext.SerializationPair
+                        .fromSerializer(tokenSerializer));
 
             return RedisCacheManager.builder(redisConnectionFactory)
-                    .cacheDefaults(tokenCacheConfig)
-                    .withCacheConfiguration("systemUserTokenCache", tokenCacheConfig)
-                    .withCacheConfiguration("userInfoCache", userInfoCacheConfig)
-                    .build();
+                .cacheDefaults(tokenCacheConfig)
+                .withCacheConfiguration("systemUserTokenCache", tokenCacheConfig)
+                .withCacheConfiguration("userInfoCache", userInfoCacheConfig)
+                // caches for functional tests
+                .withCacheConfiguration("legalRepATokenCache", tokenCacheConfig)
+                .withCacheConfiguration("caseOfficerTokenCache", tokenCacheConfig)
+                .withCacheConfiguration("adminOfficerTokenCache", tokenCacheConfig)
+                .withCacheConfiguration("homeOfficePouTokenCache", tokenCacheConfig)
+                .withCacheConfiguration("homeOfficeGenericTokenCache", tokenCacheConfig)
+                .withCacheConfiguration("judgeTokenCache", tokenCacheConfig)
+                .build();
 
         } catch (Exception e) {
             // if redis is down, dont cache make idam calls, until pod restarts
@@ -97,15 +98,8 @@ public class CacheConfiguration {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(
-            @Value("${spring.data.redis.url}") String redisUrl,
-            @Value("${spring.data.redis.secret}") String accessKey) {
-
-        if (redisUrl == null || redisUrl.isBlank()) {
-            log.warn("No Redis URL configured - falling back to Caffeine");
-            // return a dummy factory - cacheManager will catch the ping failure and fall back
-            return new LettuceConnectionFactory();
-        }
-
+        @Value("${spring.data.redis.url}") String redisUrl,
+        @Value("${spring.data.redis.secret}") String accessKey) {
         try {
             RedisURI redisUri = RedisURI.create(redisUrl);
 
@@ -127,14 +121,14 @@ public class CacheConfiguration {
             }
 
             LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                    .commandTimeout(Duration.ofSeconds(5))
-                    .useSsl()
-                    .disablePeerVerification()
-                    .build();
+                .commandTimeout(Duration.ofSeconds(5))
+                .useSsl()
+                .disablePeerVerification()
+                .build();
 
             LettuceConnectionFactory factory = new LettuceConnectionFactory(
-                    config,
-                    clientConfig
+                config,
+                clientConfig
             );
 
             factory.afterPropertiesSet();
