1ef4c20373e38a7014036acbfb337b860bef93a1
apereverzin1 <140609226+apereverzin1@users.noreply.github.com>
2026-04-23 12:24:22 +0100

    Removed secrets (#471)
    Co-authored-by: Andrey Pereverzin <andreypereverzin@Mac.broadband>

--- src/integrationTest/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/component/SpringBootIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/component/SpringBootIntegrationTest.java
@@ -26,9 +26,7 @@ import uk.gov.hmcts.reform.iahomeofficeintegrationapi.component.testutils.TestCo
     "IDAM_URL=http://127.0.0.1:8990/idam",
     "S2S_URL=http://127.0.0.1:8990/s2s",
     "IA_IDAM_CLIENT_ID=ia",
-    "IA_IDAM_SECRET=something",
     "IA_HOMEOFFICE_CLIENT_ID=ho-client-id",
-    "IA_HOMEOFFICE_SECRET=something",
     "HOME_OFFICE_ENDPOINT=http://127.0.0.1:8990"
 })
 @AutoConfigureMockMvc(addFilters = false)
