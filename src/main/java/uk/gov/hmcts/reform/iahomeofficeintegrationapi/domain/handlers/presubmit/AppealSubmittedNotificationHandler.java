9c05ec2bc10b94cedfef175888b2721fddf0be0e
Ed Graham <34963216+ed-graham@users.noreply.github.com>
2026-06-25 08:37:41 +0100

    DIAC-2620 Send new APPEAL_REQUESTED message to the Home Office (#505)
    * Send new APPEAL_REQUESTED message to the Home Office
    
    * New message type and handler now created
    
    * WIP
    
    * Added GWF capability - thanks to Makeda for spotting that
    
    * Ensuring this won't be called by accident

---
+++ src/main/java/uk/gov/hmcts/reform/iahomeofficeintegrationapi/domain/handlers/presubmit/AppealSubmittedNotificationHandler.java
@@ -0,0 +1,105 @@
+package uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.handlers.presubmit;
+
+import static java.util.Collections.emptyList;
+import static java.util.Objects.requireNonNull;
+import static uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.AppealSubmittedInstructMessage.AppealSubmittedInstructMessageBuilder.appealSubmittedInstructMessage;
+import static uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.AsylumCaseDefinition.APPEAL_REFERENCE_NUMBER;
+import static uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.AsylumCaseDefinition.HOME_OFFICE_APPELLANTS;
+import static uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.MessageType.APPEAL_REQUESTED;
+import static uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.Event.SUBMIT_APPEAL;
+
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.HomeOfficeAppellant;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.State;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.AppealSubmittedInstructMessage;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.AsylumCase;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.AsylumCaseDefinition;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.callback.Callback;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.callback.PreSubmitCallbackResponse;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.callback.PreSubmitCallbackStage;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.entities.ccd.field.IdValue;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.handlers.PreSubmitCallbackHandler;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.service.HomeOfficeInstructService;
+import uk.gov.hmcts.reform.iahomeofficeintegrationapi.domain.service.NotificationsHelper;
+
+
+@Slf4j
+@Component
+public class AppealSubmittedNotificationHandler implements PreSubmitCallbackHandler<AsylumCase> {
+
+    private HomeOfficeInstructService homeOfficeInstructService;
+    private NotificationsHelper notificationsHelper;
+
+    public AppealSubmittedNotificationHandler(
+        HomeOfficeInstructService homeOfficeInstructService,
+        NotificationsHelper notificationsHelper
+    ) {
+        this.homeOfficeInstructService = homeOfficeInstructService;
+        this.notificationsHelper = notificationsHelper;
+    }
+
+    @Override
+    public boolean canHandle(PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
+        requireNonNull(callbackStage, "callbackStage must not be null");
+        requireNonNull(callback, "callback must not be null");
+
+        return callbackStage == PreSubmitCallbackStage.ABOUT_TO_SUBMIT
+               && (callback.getEvent() == SUBMIT_APPEAL)
+               // ensure this is only called when the appeal is first submitted (rather than when it is subsequently edited)
+               && (Arrays.asList(
+                    State.APPEAL_STARTED,
+                    State.APPEAL_STARTED_BY_ADMIN
+                    ).contains(callback.getCaseDetails().getState()));               
+    }
+
+    @Override
+    public PreSubmitCallbackResponse<AsylumCase> handle(
+        PreSubmitCallbackStage callbackStage, Callback<AsylumCase> callback) {
+
+        if (!canHandle(callbackStage, callback)) {
+            throw new IllegalStateException("Cannot handle callback");
+        }
+
+        AsylumCase asylumCase = callback.getCaseDetails().getCaseData();
+
+        // Only proceed if the new  applications/v1/{id}  Home Office endpoint has already been called (and the appellant data is present)
+        Optional<List<IdValue<HomeOfficeAppellant>>> homeOfficeAppellantsOpt = asylumCase.read(HOME_OFFICE_APPELLANTS);
+        if (homeOfficeAppellantsOpt.orElse(emptyList()).isEmpty()) {
+            return new PreSubmitCallbackResponse<>(asylumCase);
+        }
+
+        log.info("Preparing to send {} notification to HomeOffice for event {}",
+            APPEAL_REQUESTED.name(), callback.getEvent());
+
+        final String homeOfficeReferenceNumber = notificationsHelper.getHomeOfficeReference(asylumCase);
+
+        final String caseId = asylumCase.read(APPEAL_REFERENCE_NUMBER, String.class)
+            .orElseThrow(() -> new IllegalStateException("Case ID for the appeal is not present"));
+
+        final AppealSubmittedInstructMessage bundleInstructMessage =
+            appealSubmittedInstructMessage()
+                .withConsumerReference(notificationsHelper.getConsumerReference(caseId))
+                .withHoReference(homeOfficeReferenceNumber)
+                .withMessageHeader(notificationsHelper.getMessageHeader())
+                .withMessageType(APPEAL_REQUESTED.name())
+                .build();
+
+        log.info("Finished constructing {} notification request for caseId: {}, HomeOffice reference: {}",
+            APPEAL_REQUESTED.name(), caseId, homeOfficeReferenceNumber);
+
+        final String notificationStatus = homeOfficeInstructService.sendNotification(bundleInstructMessage);
+
+        asylumCase.write(AsylumCaseDefinition.HOME_OFFICE_APPEAL_SUBMITTED_INSTRUCT_STATUS, notificationStatus);
+
+        log.info("SENT: {} notification for caseId: {}, HomeOffice reference: {}, status: {}, event: {}",
+            APPEAL_REQUESTED.name(), caseId, homeOfficeReferenceNumber, notificationStatus, callback.getEvent());
+
+        return new PreSubmitCallbackResponse<>(asylumCase);
+    }
+}
